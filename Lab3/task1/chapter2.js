dd6b1314645dbf462edb1e23a56a812d3b956c6f
Olzhas <niyazbekov.olzhas06@gmail.com>
2026-02-09 22:47:43 +0500

    Complete lab3

---
+++ Lab3/task1/chapter2.js
@@ -0,0 +1,15 @@
+// chapter2.1
+let name = "John";
+let  admin = name;
+
+alert(admin);
+
+// chapter2.2
+let ourPlanet = "Earth";
+let currentVisitor = "John";
+
+// chapter2.3
+const BIRTHDAY = '18.04.1982';
+const age = calculateAge(BIRTHDAY);
+
+
